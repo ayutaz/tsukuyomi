efb83f060864f7eae20c5fa1a881f278a57d2ffe
inamotoyuta <inamoto_yuta@cyberagent.co.jp>
2025-06-25 16:33:14 +0900

    Fix CUDA allocator configuration
    - Comment out max_split_size_mb in config (not supported in all PyTorch versions)
    - Remove max_split_size_mb from start_training.sh
    - This fixes RuntimeError: Unrecognized CachingAllocator option

--- scripts/start_training.sh
+++ scripts/start_training.sh
@@ -3,7 +3,7 @@
 
 # 環境変数の設定
 export CUDA_VISIBLE_DEVICES=0
-export PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:128
+export PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6
 
 # T4 GPU用の最適化設定
 export TORCH_CUDA_ARCH_LIST="7.5"
